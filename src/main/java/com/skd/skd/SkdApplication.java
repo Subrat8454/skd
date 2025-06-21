371a6151e05474fdc730bbf9038ec6c8fabebf79
Subrat8454 <beherasubrat729@gmail.com>
2025-06-21 20:52:47 +0530

    first commit

---
+++ src/main/java/com/skd/skd/SkdApplication.java
@@ -0,0 +1,13 @@
+package com.skd.skd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkdApplication.class, args);
+	}
+
+}
