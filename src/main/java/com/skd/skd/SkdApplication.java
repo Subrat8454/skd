80db287eec4ef26307f06ccd999528dc30d11c47
Subrat8454 <beherasubrat729@gmail.com>
2025-06-21 23:34:54 +0530

    Update SkdApplication.java

--- src/main/java/com/skd/skd/SkdApplication.java
+++ src/main/java/com/skd/skd/SkdApplication.java
@@ -9,8 +9,9 @@ public class SkdApplication {
 	public static void main(String[] args) {
 		String name = "mike";
 		int x =100;
-		int y = 200;
 		boolean isPresent=false;
+		int y = 200;
+		char x = 'a';
 		SpringApplication.run(SkdApplication.class, args);
 	}
 
